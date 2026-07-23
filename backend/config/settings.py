c6ab3db6d8568c2f27a4975a657c29348b7d2b5b
MAIRA JAVED <96972779+21108130@users.noreply.github.com>
2026-07-23 15:53:39 +0500

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,7 +138,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 
-    "https://milepost-trip-planner-87xt-9p2oyg6s2-mairas-projects-74301050.vercel.app",
+    "https://milepost-trip-planner-fzqjnvmmp-mairas-projects-74301050.vercel.app/",
 ]
 
 
